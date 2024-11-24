03f6a36dc57875fd7f9f57c5a9605f57f09234a0
spyflow <133718732+spyflow@users.noreply.github.com>
2024-11-24 18:19:17 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,93 +6,126 @@ import { Heading, Text, Flex, Button, Grid, Icon, InlineCode, Arrow } from '@/on
 import Link from 'next/link';
 
 export default function Home() {
-	const skills = [
-		{
-			name: "Python",
-			logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
-		},
-		{
-			name: "C",
-			logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg",
-		},
-		{
-			name: "JavaScript",
-			logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-		},
-		{
-			name: "HTML",
-			logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
-		},
-		{
-			name: "PHP",
-			logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
-		},
-		{
-			name: "TYPESCRIPT",
-			logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-		},
-	];
+    const skills = [
+        {
+            name: "Python",
+            logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+        },
+        {
+            name: "C",
+            logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg",
+        },
+        {
+            name: "JavaScript",
+            logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+        },
+        {
+            name: "HTML",
+            logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        },
+        {
+            name: "PHP",
+            logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
+        },
+        {
+            name: "TYPESCRIPT",
+            logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+        },
+    ];
 
-	return (
-		<Flex
-			fillWidth paddingTop="l" paddingX="l"
-			direction="column" alignItems="center" flex={1}>
-			<Flex
-				as="main"
-				direction="column" justifyContent="center"
-				fillWidth fillHeight padding="l" gap="l">
-				<Heading
-					variant="body-strong-xl"
-					marginBottom="m">
-					¡Hola! Soy Spyflow
-				</Heading>
-				<Text
-					variant="body-default-m"
-					onBackground="neutral-weak"
-					marginBottom="m">
-					Tengo 20 años y me especializo en desarrollo con tecnologías como Python, C, JavaScript, HTML y PHP.
-				</Text>
-				<Heading
-					variant="body-strong-m"
-					marginBottom="m">
-					Mis Habilidades
-				</Heading>
-				<Grid
-					columns="repeat(auto-fit, minmax(120px, 1fr))"
-					gap="l"
-					padding="l">
-					{skills.map((skill) => (
-						<Flex
-							key={skill.name}
-							direction="column"
-							alignItems="center"
-							gap="m">
-							<img
-								src={skill.logo}
-								alt={`${skill.name} logo`}
-								style={{ width: '60px', height: '60px' }}
-							/>
-							<Text variant="body-strong-s">{skill.name}</Text>
-						</Flex>
-					))}
-				</Grid>
-				<Flex gap="m" marginTop="l">
-					<Button
-						href="https://github.com/spyflow"
-						prefixIcon="github"
-						size="m"
-						variant="secondary">
-						GitHub
-					</Button>
-					<Button
-						href="https://cl.linkedin.com/in/javier-ferrada-riquelme"
-						prefixIcon="linkedin"
-						size="m"
-						variant="secondary">
-						LinkedIn
-					</Button>
-				</Flex>
-			</Flex>
-		</Flex>
-	);
+    const projects = [
+        {
+            name: "Web Scrapping",
+            url: "https://video.makatunga.uy/peliculas/",
+            description: "Es una página de películas que hace scraping para obtener la información de las películas y los links de visualización.",
+            disclaimer: "No me hago responsable por el uso indebido de la página.",
+            dmca: "https://video.makatunga.uy/dmca",
+        }
+    ];
+
+    return (
+        <Flex
+            fillWidth paddingTop="l" paddingX="l"
+            direction="column" alignItems="center" flex={1}>
+            <Flex
+                as="main"
+                direction="column" justifyContent="center"
+                fillWidth fillHeight padding="l" gap="l">
+                <Heading
+                    variant="body-strong-xl"
+                    marginBottom="m">
+                    ¡Hola! Soy Spyflow
+                </Heading>
+                <Text
+                    variant="body-default-m"
+                    onBackground="neutral-weak"
+                    marginBottom="m">
+                    Tengo 20 años y me especializo en desarrollo con tecnologías como Python, C, JavaScript, HTML y PHP.
+                </Text>
+                <Heading
+                    variant="body-strong-m"
+                    marginBottom="m">
+                    Mis Habilidades
+                </Heading>
+                <Grid
+                    columns="repeat(auto-fit, minmax(120px, 1fr))"
+                    gap="l"
+                    padding="l">
+                    {skills.map((skill) => (
+                        <Flex
+                            key={skill.name}
+                            direction="column"
+                            alignItems="center"
+                            gap="m">
+                            <img
+                                src={skill.logo}
+                                alt={`${skill.name} logo`}
+                                style={{ width: '60px', height: '60px' }}
+                            />
+                            <Text variant="body-strong-s">{skill.name}</Text>
+                        </Flex>
+                    ))}
+                </Grid>
+                <Heading
+                    variant="body-strong-m"
+                    marginBottom="m">
+                    Proyectos
+                </Heading>
+                <Grid
+                    columns="repeat(auto-fit, minmax(120px, 1fr))"
+                    gap="l"
+                    padding="l">
+                    {projects.map((project) => (
+                        <Flex
+                            key={project.name}
+                            direction="column"
+                            alignItems="center"
+                            gap="m">
+                            <Text variant="body-strong-s">{project.name}</Text>
+                            <Text variant="body-default-m">{project.description}</Text>
+                            <Text variant="body-default-s" style={{ color: 'red' }}>{project.disclaimer}</Text>
+                            <Link href={project.url}>Visita el proyecto</Link>
+                            <Link href={project.dmca}>DMCA</Link>
+                        </Flex>
+                    ))}
+                </Grid>
+                <Flex gap="m" marginTop="l">
+                    <Button
+                        href="https://github.com/spyflow"
+                        prefixIcon="github"
+                        size="m"
+                        variant="secondary">
+                        GitHub
+                    </Button>
+                    <Button
+                        href="https://cl.linkedin.com/in/javier-ferrada-riquelme"
+                        prefixIcon="linkedin"
+                        size="m"
+                        variant="secondary">
+                        LinkedIn
+                    </Button>
+                </Flex>
+            </Flex>
+        </Flex>
+    );
 }
